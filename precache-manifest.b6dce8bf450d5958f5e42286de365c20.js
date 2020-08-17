ab75cd8796ffd2d85f20fda72e8cb5447a159a99
Ife Lawal <il693@nyu.edu>
2020-08-16 22:43:14 -0400

    Updates

---
+++ precache-manifest.b6dce8bf450d5958f5e42286de365c20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84a0cfaa24f795b70f70f3036464545",
+    "url": "/forecasting-sliders-react/index.html"
+  },
+  {
+    "revision": "1ceed9a273e9ec761276",
+    "url": "/forecasting-sliders-react/static/css/main.dfffb338.chunk.css"
+  },
+  {
+    "revision": "9d534c5b78a8d1c935c1",
+    "url": "/forecasting-sliders-react/static/js/2.9ad04650.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/forecasting-sliders-react/static/js/2.9ad04650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ceed9a273e9ec761276",
+    "url": "/forecasting-sliders-react/static/js/main.8e64bb7b.chunk.js"
+  },
+  {
+    "revision": "4e6311b4b27225897d5b",
+    "url": "/forecasting-sliders-react/static/js/runtime-main.322a15cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/forecasting-sliders-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
